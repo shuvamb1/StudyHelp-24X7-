b224cc9365495e78f798539b85e87bf6e980c47a
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-07-10 20:20:28 +0530

    Add files via upload

---
+++ script15.js
@@ -0,0 +1,30 @@
+function downloadFile(url, filename) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Event listener for the button click
+document.getElementById('nett').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1O1ZLB3K4TE-aSNdC_olSZtH4P71YzCm5'; // Change this to the correct file path
+    const fileName = 'Microprocessor Theory.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('netp').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id='; // Change this to the correct file path
+    const fileName = 'Microprocessor Practical.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('netbooks').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1f9YQLjWQA8z9slmizlFaM49Zl2FaR0Nt'; // Change this to the correct file path
+    const fileName = 'Microprocessor Books.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('netpyq').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id='; // Change this to the correct file path
+    const fileName = 'Microprocessor PYQ.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
