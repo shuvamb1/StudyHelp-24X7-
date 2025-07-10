bbc7ecb5a47eb381b2dc32077efc8e2a641d34f2
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-07-10 19:18:57 +0530

    Add files via upload

---
+++ script13.js
@@ -0,0 +1,30 @@
+function downloadFile(url, filename) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Event listener for the button click
+document.getElementById('aaal').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1O1ZLB3K4TE-aSNdC_olSZtH4P71YzCm5'; // Change this to the correct file path
+    const fileName = 'Microprocessor Theory.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('ssal').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id='; // Change this to the correct file path
+    const fileName = 'Microprocessor Practical.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('albooks').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1f9YQLjWQA8z9slmizlFaM49Zl2FaR0Nt'; // Change this to the correct file path
+    const fileName = 'Microprocessor Books.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
+document.getElementById('alpyq').addEventListener('click', function() {
+    const fileUrl = 'https://drive.google.com/uc?export=download&id='; // Change this to the correct file path
+    const fileName = 'Microprocessor PYQ.zip'; // Specify the file name
+    downloadFile(fileUrl, fileName);
+});
