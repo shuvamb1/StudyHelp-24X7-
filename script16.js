ff6151c4d17a3482d6920f6edccfb8b0e62f0b6f
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-12-02 10:22:33 +0530

    Update script16.js

--- script16.js
+++ script16.js
@@ -11,12 +11,12 @@ function downloadFile(url, filename) {
 // Event listener for the button click
 document.getElementById('netpyq').addEventListener('click', function() {
     const fileUrl = 'https://drive.google.com/uc?export=download&id=1VEzAVZm4Cqv069XQwFZKLgETmb8rMTTg'; // Change this to the correct file path
-    const fileName = 'Maths PYQ.zip'; // Specify the file name
+    const fileName = 'probability PYQ.zip'; // Specify the file name
     downloadFile(fileUrl, fileName);
 });
 document.getElementById('net1pyq').addEventListener('click', function() {
     const fileUrl = 'https://drive.google.com/uc?export=download&id=1VEzAVZm4Cqv069XQwFZKLgETmb8rMTTg'; // Change this to the correct file path
-    const fileName = 'Maths PYQ.zip'; // Specify the file name
+    const fileName = 'calculas PYQ.zip'; // Specify the file name
     downloadFile(fileUrl, fileName);
 });
 document.getElementById('logo').addEventListener('click', function() {
