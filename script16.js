f983f32b3fbe0b1256971808ec83cf9f197d6a1d
shuvamb1 <shuvambhattacharya896@gmail.com>
2025-12-02 10:29:39 +0530

    Update script16.js

--- script16.js
+++ script16.js
@@ -10,12 +10,12 @@ function downloadFile(url, filename) {
 
 // Event listener for the button click
 document.getElementById('netpyq').addEventListener('click', function() {
-    const fileUrl = 'https://drive.google.com/uc?export=download&id=1VEzAVZm4Cqv069XQwFZKLgETmb8rMTTg'; // Change this to the correct file path
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1CV-Dw4cM1MbuHAO0jIeGvLDX46MutAC7'; // Change this to the correct file path
     const fileName = 'probability PYQ.zip'; // Specify the file name
     downloadFile(fileUrl, fileName);
 });
 document.getElementById('net1pyq').addEventListener('click', function() {
-    const fileUrl = 'https://drive.google.com/uc?export=download&id=1VEzAVZm4Cqv069XQwFZKLgETmb8rMTTg'; // Change this to the correct file path
+    const fileUrl = 'https://drive.google.com/uc?export=download&id=1UpDJRhe8PkMyqjkKRGt4bcXVzAP7Pb3a'; // Change this to the correct file path
     const fileName = 'calculas PYQ.zip'; // Specify the file name
     downloadFile(fileUrl, fileName);
 });
